15308e09877cccffb6468c476f558dace787f1c8
Joe Clark <weirdlookingjay@live.com>
2022-11-25 14:40:06 -0500

    Created Pages and Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ForgotPassword, Home, Offers, Profile, SignIn, SignUp } from "./pages";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Realtor Web App</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import Home from "./Home";
+import Profile from "./Profile";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+import ForgotPassword from "./ForgotPassword";
+import Offers from "./Offers";
+
+export { Home, Profile, SignIn, SignUp, ForgotPassword, Offers };
